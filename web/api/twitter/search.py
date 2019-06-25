8d48187d53631eb62337ec25e43ae2f2d36d3f2c
Carlos A. Ospina <carlosaospinac@gmail.com>
2019-06-24 19:45:33 -0500

    NBC is included in retrieved tweets from API.

--- web/api/twitter/search.py
+++ web/api/twitter/search.py
@@ -7,11 +7,15 @@ from classifiers.nbc import NaiveBayesClassifier
 class TwitterSearch(API):
     def __init__(self):
         self.twitter = TwitterAPI()
+        self.nbc = NaiveBayesClassifier()
 
     def get(self, request):
-        q = request.raw_args.get("q", "")
-        count = request.raw_args.get("count", 12)
-        lang = request.raw_args.get("lang", "en")
+        q = request.raw_args.get('q', "")
+        count = request.raw_args.get('count', 12)
+        lang = request.raw_args.get('lang', "en")
+        classify = request.raw_args.get('classify', "false")
+        class_field = request.raw_args.get('class_field', "text")
+
         if q == "":
             return self.response(400)
 
@@ -20,6 +24,14 @@ class TwitterSearch(API):
             lang=lang,
             count=count
         )
+        if classify == "true":
+            for tweet in r:
+                text = tweet.get(class_field)
+                if not text:
+                    continue
+
+                tweet['nbc'] = self.nbc.classify_naive_bayes_text(text)
+
         return self.json(data=r, count=count)
 
 
